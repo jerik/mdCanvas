cc465ba6d7e3620710fad53eb270f8f2b66446dc
Erik Heinz <jerik@gmx.net>
2017-05-28 23:55:00 +0200

    added promises, refactored

--- js/modules/mdCanvas.js
+++ js/modules/mdCanvas.js
@@ -1,59 +1,59 @@
 export class main { 
 
-		constructor(  ) { 
-			this.init(  );
-			this.say = "This is a es6 main test module.";
+	constructor(  ) { 
+		this.init(  );
+		this.say = "This is a es6 main test module.";
+		this.canvas = { first: "item" }
+	}
+
+	// for integration tests 
+	// @todo integration test not possible as getFile has no return result...
+	init( url ) { 
+		// windows.location 		contains the full url 	> http://www.test.de?foo=bar&here=wegoagain
+		// windows.location.search 	contains the parameters > 					?foo=bar&here=wegoagain
+		// windows.location.search.substring( 1 ) removes the ? from the parameters
+		var url = url || window.location.search.substring( 1 ); // get the test url or the real parameters
+		var filename = this.getFilenameFrom( url );
+
+		var mdPath = filename + '.md';
+
+		// resolve function 
+		var doResolve = function ( data ) { 
+			console.log( "doResolve :", data );
+			return data; // important for the next promise in chain
 		}
 
-		// for integration tests 
-		// @todo integration test not possible as getFile has no return result...
-		init( url ) { 
-			// windows.location 		contains the full url 	> http://www.test.de?foo=bar&here=wegoagain
-			// windows.location.search 	contains the parameters > 					?foo=bar&here=wegoagain
-			// windows.location.search.substring( 1 ) removes the ? from the parameters
-			var url = url || window.location.search.substring( 1 ); // get the test url or the real parameters
-			var filename = this.getFilenameFrom( url );
+		// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Functions/Pfeilfunktionen
+		var doResolveNow = ( data ) => {  // doResolveNow should be doResolve !!
+			this.parse( data );
+			// console.log( "doResolveNow :", data );
+			return data; // important for the next promise in chain
+		}
 
-			// https://developers.google.com/web/fundamentals/getting-started/primers/promises
-			// old working version, but should be replaced
-			// this.getFile(filename);
+		var doReject = function( data ) { 
+			console.log( "Reject: ", data );
+		}
 
-			var mdPath = filename + '.md';
-
-			var doResolve = function ( data ) { 
-				console.log( "doResolve :", data );
-				return data; // important for the next promise in chain
-			}
-
-			var doResolveNow = function ( data ) { 
-				console.log( "doResolveNow :", data );
-				return data; // important for the next promise in chain
-			}
-
-			var doReject = function( data ) { 
-				console.log( "Reject: ", data );
-			}
-
-			this.ajax( mdPath )
-				.then( 
-					doResolve,
-					/* function( data ) { 
-						var tmp = data.replace( 'content', 'BAAAR' );
-						console.log( "2. resolve: ", tmp );
-						return tmp;
-					}, */
-					doReject
-					/* function( data ) { // reject
-						console.log( "reject: ", data );
-					} */
-				)
-				.then(  // work on the first promise result
-					doResolveNow, 
-					doReject
-				).catch( function( error ) { 
-					console.error( "Catched: ", error )
-				} );
-	}
+		this.ajax( mdPath )
+			.then( 
+				doResolve,
+				/* function( data ) { 
+					var tmp = data.replace( 'content', 'BAAAR' );
+					console.log( "2. resolve: ", tmp );
+					return tmp;
+				}, */
+				doReject
+				/* function( data ) { // reject
+					console.log( "reject: ", data );
+				} */
+			)
+			.then(  // work on the first promise result
+				doResolveNow, 
+				doReject
+			).catch( function( error ) { 
+				console.error( "Catched: ", error )
+			} );
+}
 
 	getUrlParams( url ) {  // parameter is for testing purpose
 		// if url contains only "bar=foo&this=that", the follwing sliece does work and seams to return the untouched url. Thats ok for now.
@@ -104,91 +104,36 @@ export class main {
 	 * 	 ...
 	 * }
 	 */
-	getFile( filename ) { 
-		var pathmd = filename + ".md";
-		var content = "";
-		// @todo here I have to dig in!!! with closure?
-		// http://javascriptissexy.com/understand-javascript-closures-with-ease/
-		// https://oli.me.uk/2013/06/29/equipping-vim-for-javascript/
-		var foobar = "";
-
-		// @todo need to merge
-		var parse = function(content) {
-			// console.log("do the parsing on: " + content)
-			foobar = "parse";
-			getSections(content);
-		}
-
-		// @todo need to merge
-		var getSections = function( content ) { 
-			if ( content == undefined ) { 
-				return new Error( "Missing parameter 'content'" );
-			}
-			var parts = content.split( '#' ); // @todo works only is not # is used as numbere list
-			foobar = "getSections";
-
-			// tidy up array, remove first empty entry
-			if ( Array.isArray( parts ) && parts.length > 1) { 
-				if ( parts[0] == "" ) { 
-					parts.shift(  );
-				}
-			}
-			console.log( parts );
-			//return parts;
-			buildData(parts);
-
-		}
-
-		// @todo 20170426 create dataObject 
-		// @todo need to merge
-		var getHeading = function( parts ) { 
-			var heading = parts.slice( 0, parts.indexOf("\n" ));
-			console.log( 'Heading: ' + heading );
-			foobar = "getHeading";
-			return heading;
-
-			// @todo bastel object with heading and content: { mdc-costs: "this is the \n text of the box" }
-		}
-
-		// @todo need to merge
-		var buildData = function(parts) {
-			foobar = "buildData";
-			getHeading(parts);
-
-		}
-
-		// @todo can be removed obsolet
-		$.get( pathmd, function( data ) { 
-			//console.log( data );
-			//return data;
-			content = data;
-		}, 'text' )
-		.done( function(  ){ 
-			parse( content );
-			console.log('get.done: ' + foobar);
-			return foobar;
-		} );
+	parse( content ) { 
+		// run parse
+		var parts = this.getSections(content);
+		var heading = this.getHeading( parts );
+		console.log( this.canvas );
 	}
 
-	// https://davidwalsh.name/write-javascript-promises
-	// http://www.htmlgoodies.com/beyond/javascript/making-promises-with-jquery-deferred.html
-	// http://joseoncode.com/2011/09/26/a-walkthrough-jquery-deferred-and-promise/
-	// https://developers.google.com/web/fundamentals/getting-started/primers/promises
-	getFilePromise( filename ) { 
-		var pathmd = filename + ".md";
+	getSections( content ) { 
+		if ( content == undefined ) { 
+			return new Error( "Missing parameter 'content'" );
+		}
+		var parts = content.split( '#' ); // @todo works only is not # is used as numbere list
 
-		//return new Promise(resolve, reject) { }
-		$.get( pathmd, function( data ) { 
-			//console.log( data );
-			//return data;
-			content = data;
-		}, 'text' )
-		.done( function(  ){ 
-			console.log('get.done: ' + content);
-			return content;
-		} );
+		// tidy up array, remove first empty entry
+		if ( Array.isArray( parts ) && parts.length > 1) { 
+			if ( parts[0] == "" ) { 
+				parts.shift(  );
+			}
+		}
+		console.log( "THIS Parts: ", parts );
+		return parts;
+		//this.getHeading( parts );
 	}
 
+	getHeading( parts ) { 
+		var heading = parts.slice( 0, parts.indexOf("\n" ));
+		console.log( 'HEading: ' + heading );
+		return heading;
 
-
+		// @todo bastel object with heading and content: { mdc-costs: "this is the \n text of the box" }
+		// http://2ality.com/2014/08/es6-today.html
+	}
 }
